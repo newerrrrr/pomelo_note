fea47a1a0bec0684ea93d492a584ee497403f909
hlb <hlb@163.com>
2019-06-22 20:28:47 +0800

    ..

---
+++ project/client/HLBJS/assets/script/Logo.js
@@ -0,0 +1,64 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+        pomelo.init({
+            host : "192.168.159.128",
+            port : 3014,
+        }, function () {
+            var route = 'gate.gateHandler.queryEntry';
+            pomelo.request(route, {
+                username:"huanglibo",
+                rid:1234,
+            }, function (data) {
+                console.log("data======================");
+                // pomelo.disconnect(function () {
+                //     pomelo.init({
+                //         host : host2,
+                //         host : host2,
+                //         port : port2,
+                //         reconnect : true
+                //     }, function () {
+                //     })
+                // });
+            })
+            
+        });
+
+    },
+
+    // update (dt) {},
+});
